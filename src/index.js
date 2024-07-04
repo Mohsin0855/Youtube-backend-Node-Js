de1fccae86527f96362f36ef1d1d43d72b80f040
Mohsin Naseer <chmohsin.official@gmail.com>
2024-07-04 15:40:22 +0500

    db connection

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv';
+
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+/*
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+*/
+/*
+import express from "express";
+const app = express()
+
+( async() =>{
+    try {
+        await  mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`,
+            app.on("error",(error)=>{
+                console.log("Error", error)
+                throw error
+            }),
+            app.listen(process.env.PORT,()=>{
+                console.log(`port is listening on ${process.env.PORT}`)
+            })
+        )
+    } 
+     catch (error) {
+        console.error("Server Error:", error)
+        throw error
+    }
+})
+()
+*/
